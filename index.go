004366d4f579f099cd84d6076123bc4fb01fae4a
Adrian Kuendig <adriankue@gmail.com>
2012-05-16 20:31:53 +0200

    initial files

---
+++ index.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "github.com/hoisie/web"
+)
+
+func hello(ctx *web.Context, val string) {
+    for k,v := range ctx.Params {
+        println(k, v)
+    }
+}
+
+func main() {
+    web.Get("/(.*)", hello)
+    web.Run("0.0.0.0:9999")
+}
